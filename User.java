452d73ef61ee7e79bc5b2d0c6f14e7e4d26b59e8
Luuuuukas <75544324+Luuuuukas@users.noreply.github.com>
2023-02-05 13:48:24 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 56130
+ */
 public class User {
 
-    private  int id ;
-    private String firstName; //454
-    private String lastName;
-    private int age;
+    private  int​ id​ ;
+    private​ String firstName​;
+     private​ String lastName​;
+     private​ int age;
     private String profession;
     private ArrayList<User> children;
 
