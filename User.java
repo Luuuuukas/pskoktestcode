5e8bc62b35e0a58b30b83f6b2074f1adb3063c95
Luuuuukas <75544324+Luuuuukas@users.noreply.github.com>
2023-02-05 14:37:13 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,26 +1,23 @@
 import java.util.ArrayList;
 public class User {
-
     private final int id;
     private final String firstUserName;
     private final String lastUserName;
     private final int age;
     private ArrayList<User> children;
 
-    public User(int id, String firstName, String lastName, int age) {
-
+    public User(int id, String firstUserName, String lastUserName, int age) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstUserName = firstUserName;
+        this.lastUserName = lastUserName;
         this.age = age;
-        this.profession = profession;
     }
-    
+
     public static void main(String[]args){
     }
 
     public String getLastName() {
-        return lastName;
+        return lastUserName;
     }
-    
+
 }
